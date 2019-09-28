9e68c838267d4a746703c79783271ee7728a8f53
Darayavaus <darayavaus.chern@yandex.ru>
2019-09-28 08:42:49 +0300

    Updates

---
+++ precache-manifest.ff57a5308bcdeb063ec41ae0b7ca8e86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872a4557f00e91a605f5f426ba58d220",
+    "url": "/vk-mini-app/index.html"
+  },
+  {
+    "revision": "2fc5ec110dcd9b4ea54e",
+    "url": "/vk-mini-app/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "240912e3cbba31655b72",
+    "url": "/vk-mini-app/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2fc5ec110dcd9b4ea54e",
+    "url": "/vk-mini-app/static/js/2.66b217b8.chunk.js"
+  },
+  {
+    "revision": "240912e3cbba31655b72",
+    "url": "/vk-mini-app/static/js/main.cff506a0.chunk.js"
+  },
+  {
+    "revision": "0405a5d6d4b3359aaae5",
+    "url": "/vk-mini-app/static/js/runtime-main.4d7eee86.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-mini-app/static/media/persik.4e1ec840.png"
+  }
+]);

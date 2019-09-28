9d4037212215b46ab60a0d05e5ee11b6625cf610
Vladimir Krutkin <krutkinvs@gmail.com>
2019-09-28 14:05:46 +0300

    Event info card

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import UsersStack from '@vkontakte/vkui/dist/components/UsersStack/UsersStack';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
 import './Home.css';
@@ -16,6 +19,26 @@ const Home = ({ id, location, fetchedUser }) => (
           options={{preset: 'islands#redCircleDotIcon'}}
         />
       </Map>
+      <Div className="event-short-info">
+        <div style={{
+          backgroundImage: 'linear-gradient(135deg, #f24973 0%, #3948e6 100%)',
+          height: 200,
+          display: 'flex',
+          alignItems: 'flex-end',
+          justifyContent: 'center',
+          paddingBottom: '6px',
+          borderRadius: 12
+        }}>
+          <UsersStack
+            photos={[
+              'https://sun9-19.userapi.com/c851232/v851232757/fb949/4rDdDHqGglQ.jpg?ava=1',
+              'https://sun9-3.userapi.com/c851536/v851536176/a9b1d/xdPOltpVQRI.jpg?ava=1',
+              'https://sun9-21.userapi.com/c851416/v851416327/be840/bnUHAblZoBY.jpg?ava=1'
+            ]}
+            style={{ color: "#fff" }}
+          >{location}</UsersStack>
+        </div>
+      </Div>
     </YMaps>
   </Panel>
 );

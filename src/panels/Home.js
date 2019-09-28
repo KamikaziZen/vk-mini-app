9cc55589b041d1f1dd02cf27ee66f853b072961e
Vladimir Krutkin <krutkinvs@gmail.com>
2019-09-28 11:55:09 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,41 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
 import './Home.css';
 
 const Home = ({ id, go, fetchedUser }) => (
-	<Panel id={id}>
-		{/* {fetchedUser &&
-		<Group title="User Data Fetched with VK Connect">
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>} */}
-
-        <YMaps>
-            <Map className="map" defaultState={{ center: [55.75, 37.57], zoom: 9 }}>
-                <Placemark geometry={[55.75, 37.57]} />
-            </Map>
-        </YMaps>	
-	</Panel>
+  <Panel id={id}>
+    <PanelHeader>Карта событий</PanelHeader>
+    <YMaps>
+      <Map className="map" defaultState={{ center: [55.75, 37.57], zoom: 9 }}>
+        <Placemark geometry={[55.75, 37.57]} />
+      </Map>
+    </YMaps>
+  </Panel>
 );
 
 Home.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
-	fetchedUser: PropTypes.shape({
-		photo_200: PropTypes.string,
-		first_name: PropTypes.string,
-		last_name: PropTypes.string,
-		city: PropTypes.shape({
-			title: PropTypes.string,
-		}),
-	}),
+  id: PropTypes.string.isRequired,
+  go: PropTypes.func.isRequired,
+  fetchedUser: PropTypes.shape({
+    photo_200: PropTypes.string,
+    first_name: PropTypes.string,
+    last_name: PropTypes.string,
+    city: PropTypes.shape({
+      title: PropTypes.string,
+    }),
+  }),
 };
 
 export default Home;

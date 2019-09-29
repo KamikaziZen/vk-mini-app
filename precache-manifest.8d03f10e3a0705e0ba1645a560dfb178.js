f07e2352d5c978651d47ef9f9a22e8b29d1b6013
Darayavaus <darayavaus.chern@yandex.ru>
2019-09-29 03:28:36 +0300

    Updates

--- precache-manifest.8d03f10e3a0705e0ba1645a560dfb178.js
+++ precache-manifest.8d03f10e3a0705e0ba1645a560dfb178.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ad01bd1afe9eaa1cb9d3088ffc6ace83",
+    "revision": "92d6034ba4afa5c3831b4bea8fe9d9ec",
     "url": "/vk-mini-app/index.html"
   },
   {
-    "revision": "d40ea959ff01d723076c",
+    "revision": "704d031a00464028f6df",
     "url": "/vk-mini-app/static/css/2.4e89ce74.chunk.css"
   },
   {
-    "revision": "1af928743ac381c3ecbf",
+    "revision": "ec078165e855b4ccf532",
     "url": "/vk-mini-app/static/css/main.f8c977cc.chunk.css"
   },
   {
-    "revision": "d40ea959ff01d723076c",
-    "url": "/vk-mini-app/static/js/2.c7dd72dc.chunk.js"
+    "revision": "704d031a00464028f6df",
+    "url": "/vk-mini-app/static/js/2.751c73d3.chunk.js"
   },
   {
-    "revision": "1af928743ac381c3ecbf",
-    "url": "/vk-mini-app/static/js/main.584ce49a.chunk.js"
+    "revision": "ec078165e855b4ccf532",
+    "url": "/vk-mini-app/static/js/main.30212a60.chunk.js"
   },
   {
     "revision": "783f846214adcbbe2646",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-mini-app/precache-manifest.f025dee9734ecf4c2d6281d5c2759f24.js"
+  "/vk-mini-app/precache-manifest.8d03f10e3a0705e0ba1645a560dfb178.js"
 );
 
 self.addEventListener('message', (event) => {
